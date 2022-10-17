61269fd77c69641180a9e6a0b23b5ff4af253521
Matheus Ambrosio <ceifa13@outlook.com>
2022-10-17 16:43:33 -0300

    choices

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ import cssIcon from "../assets/css-3.png";
 import javascriptIcon from "../assets/java-script.png";
 import reactIcon from "../assets/react.png";
 import unityIcon from "../assets/unity.png";
+import game from "../assets/game.jpg";
 
 function Home() {
   return (
@@ -27,45 +28,86 @@ function Home() {
           </div>
         </div>
       </section>
-      <section className="bg-[rgba(0,0,0,0.7)]    py-5 backdrop-blur-sm text-center">
-        <div className="">
-          <h2 className="text-4xl mb-12">Technologies</h2>
-          <article className="grid grid-flow-col justify-around">
-            <div className="group flex flex-col items-center">
-              <img src={htmlIcon} className="w-10" alt="html logo" />
-              <p className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-xl font-extralight ">Html</p>
+      <div className="backdrop-blur-sm divide-y divide-[rgba(255,255,255,0.2)] bg-[rgba(0,0,0,0.7)]">
+        <section className="py-5 text-center">
+          <div className="">
+            <h2 className="text-4xl mb-12">Technologies</h2>
+            <article className="grid grid-flow-col justify-around">
+              <div className="group flex flex-col items-center">
+                <img src={htmlIcon} className="w-10" alt="html logo" />
+                <p className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-xl font-extralight ">
+                  Html
+                </p>
+              </div>
+              <div className="group flex flex-col items-center">
+                <img src={cssIcon} className="w-10" alt=" Css logo" />
+                <p className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-xl font-extralight ">
+                  CSS
+                </p>
+              </div>
+              <div className="group flex flex-col items-center">
+                <img
+                  src={javascriptIcon}
+                  className="w-10"
+                  alt="java Script Logo "
+                />
+                <p className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-xl font-extralight ">
+                  JavaScript
+                </p>
+              </div>
+              <div className="group flex flex-col items-center">
+                <img src={reactIcon} className="w-10" alt="react logo" />
+                <p className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-xl font-extralight ">
+                  React
+                </p>
+              </div>
+              <div className="group flex flex-col items-center">
+                <img
+                  src={unityIcon}
+                  className="w-10 bg-[rgba(255,255,255,0.8)]"
+                  alt="Unity engine logo "
+                />
+                <p className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-xl font-extralight ">
+                  Unity
+                </p>
+              </div>
+              <div className="group flex flex-col items-center">
+                <img src={csharp} className="w-10" alt="C Sharp logo" />
+                <p className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-xl font-extralight ">
+                  C#
+                </p>
+              </div>
+            </article>
+          </div>
+        </section>
+
+        <section className="flex justify-center items-center   text-center overflow-hidden">
+            <div className=" flex flex-col justify-center h-[700px]  w-full  ">
+          <div className=" flex h-[300px]  gap-[200px] justify-center">
+              <div className=" hover:scale-105 transition-all duration-300 flex flex-col items-center justify-center w-[450px]">
+                <h2 className="text-4xl font-bold mb-6 bg-[rgba(255,0,0,0.5)] w-auto rounded-full px-6">Games</h2>
+                <p className="text-md  ">
+                  Lorem Ipsu dolor set ipsum lorem Ipsu dolor set ipsum Lorem
+                  Ipsu Lorem Ipsu dolor set ipsum lorem Ipsu dolor set ipsum
+                  Lorem
+                </p>
+              </div>
+              <span className="text-4xl font-bold flex flex-col justify-center">
+                or
+              </span>
+
+              <div className=" hover:scale-105 transition-all duration-300 flex flex-col items-center  justify-center w-[450px]">
+                <h2 className="text-4xl font-bold mb-6 bg-[rgba(0,4,255,0.5)] w-auto px-6 rounded-full ">web/mobile</h2>
+                <p className="text-md  ">
+                  Lorem Ipsu dolor set ipsum lorem Ipsu dolor set ipsum Lorem
+                  Ipsu Lorem Ipsu dolor set ipsum lorem Ipsu dolor set ipsum
+                  Lorem
+                </p>
+              </div>
             </div>
-            <div className="group flex flex-col items-center">
-              <img src={cssIcon} className="w-10" alt=" Css logo" />
-              <p className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-xl font-extralight ">CSS</p>
-            </div>
-            <div className="group flex flex-col items-center">
-              <img
-                src={javascriptIcon}
-                className="w-10"
-                alt="java Script Logo "
-              />
-              <p className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-xl font-extralight ">JavaScript</p>
-            </div>
-            <div className="group flex flex-col items-center">
-              <img src={reactIcon} className="w-10" alt="react logo" />
-              <p className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-xl font-extralight ">React</p>
-            </div>
-            <div className="group flex flex-col items-center">
-              <img
-                src={unityIcon}
-                className="w-10 bg-[rgba(255,255,255,0.8)]"
-                alt="Unity engine logo "
-              />
-              <p className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-xl font-extralight ">Unity</p>
-            </div>
-            <div className="group flex flex-col items-center">
-              <img src={csharp} className="w-10" alt="C Sharp logo" />
-              <p className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-xl font-extralight ">C#</p>
-            </div>
-          </article>
-        </div>
-      </section>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
